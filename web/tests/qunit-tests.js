db8627233d5b2df31f0a6d86aa7de7cfcf7c5436
Devendra <devendra@udgama.com>
2013-04-26 15:53:34 +0530

    adding qunit files and tests for tests on testswarm

---
+++ web/tests/qunit-tests.js
@@ -0,0 +1,28 @@
+var pubnub = PUBNUB.init({
+    publish_key   : 'demo',
+    subscribe_key : 'demo'
+});
+
+asyncTest("UUID response", function() {
+    expect(2);
+    pubnub.uuid(function(uuid){
+            ok(uuid, "UUID response");
+            ok(uuid.length > 10, "UUID Long Enough");
+            start();
+        }
+    });
+});
+
+
+asyncTest("publish", function() {
+    expect(1);
+    pubnub.publish({
+        channel: 'channel-a',
+        message: { test: "test" },
+        callback: function (response) {
+            deepEqual(response[0], 1);
+            start();
+        }
+    });
+
+});
