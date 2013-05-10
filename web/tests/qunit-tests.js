b29b123880e33b48b159ec8628934fe3bbe0ba4d
Devendra <devendra@udgama.com>
2013-05-10 21:21:15 +0530

    testing

--- web/tests/qunit-tests.js
+++ web/tests/qunit-tests.js
@@ -34,7 +34,7 @@ asyncTest("publish() should publish strings without error", function() {
         connect : function(response)  {
             pubnub.publish({channel: ch, message: message_string,
                 callback : function(response) {
-                    deepEqual(response[0],1);
+                    deepEqual(response[0],2);
                 }
             });
         },
