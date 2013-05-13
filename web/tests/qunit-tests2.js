605d78e9b2ddd2e467e8a4a05a2b341145204746
Devendra <devendra@udgama.com>
2013-05-13 10:04:36 +0530

    debug

--- web/tests/qunit-tests2.js
+++ web/tests/qunit-tests2.js
@@ -54,7 +54,6 @@ test("publish() should publish json array without error", function() {
     var ch = channel + '-' + ++count;
     pubnub.subscribe({ channel : ch,
         connect : function(response)  {
-            alert('CONNECT');
             pubnub.publish({channel: ch, message: message_jsona,
                 callback : function(response) {
                     deepEqual(response[0],1);
@@ -97,18 +96,18 @@ test("#here_now() should show occupancy 1 when 1 user subscribed to channel", fu
     var ch = channel + '-' + 'here-now' ;
     pubnub.subscribe({channel : ch ,
         connect : function(response) {
-            alert('CONNECT');
-            alert(response);
+            window.alert('CONNECT');
+            window.console.log(response);
             setTimeout(function() {
                 pubnub.here_now( {channel : ch, callback : function(data) {
-                    alert('Here Now');
-                    alert(data);
+                    window.alert('Here Now');
+                    window.console.log(data);
                     deepEqual(data.occupancy, 1);
                     start();
                     pubnub.publish({channel: ch , message : message_jsona,
                         callback : function(response) {
-                            alert('PUBLISH');
-                            alert(response);
+                            window.alert('PUBLISH');
+                            window.console.log(response);
                             deepEqual(response[0],1);
                             start();
                         }
@@ -117,8 +116,8 @@ test("#here_now() should show occupancy 1 when 1 user subscribed to channel", fu
                 }, 10000);
         },
         callback : function(response) {
-            alert('CALLBACK');
-            alert(response);
+            window.alert('CALLBACK');
+            window.console.log(response);
             deepEqual(response, message_jsona);
             start();
             pubnub.unsubscribe({channel : ch});
