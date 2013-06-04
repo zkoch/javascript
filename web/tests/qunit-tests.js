88f70090f8f2906f0c93a23e3be83f5dc8f73ac4
Devendra <devendra@udgama.com>
2013-06-04 23:48:03 +0530

    test commit

--- web/tests/qunit-tests.js
+++ web/tests/qunit-tests.js
@@ -99,7 +99,7 @@ asyncTest("#here_now() should show occupancy 1 when 1 user subscribed to channel
         connect : function(response) {
             setTimeout(function() {
                 pubnub.here_now( {channel : ch, callback : function(data) {
-                    equal(data.occupancy, 3);
+                    equal(data.occupancy, 1);
                     start();
                     pubnub.unsubscribe({channel : ch});
                 }})}, 10000
